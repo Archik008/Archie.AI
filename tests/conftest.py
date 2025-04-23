58c7fec80e8f9fea756679f350a821d507666a64
Артем <119063410+Archik008@users.noreply.github.com>
2025-04-23 09:01:31 +0500

    Fixed propmpt

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,9 @@ import pytest_asyncio
 from tests.test_database import create_test_database, drop_test_database
 from httpx import ASGITransport, AsyncClient
 from configure.pyconfig import MODE
+from database.database import get_db
+from tests.test_database import override_get_db
+
 
 if MODE == "DEV":
     from dev.dev_main import app
@@ -10,8 +13,6 @@ elif MODE == "PROD":
 else:
     raise Exception("Неизвестный параметр в pyconfig(MODE)")
 
-from database.database import get_db
-from tests.test_database import override_get_db
 
 TEST_INIT_DATA = "query_id=AAGgWik8AAAAAKBaKTxyylx5&user=%7B%22id%22%3A1009343136%2C%22first_name%22%3A%22%D0%90%D1%80%D1%82%D0%B5%D0%BC%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22ChristMyLife2008%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2FkU0kiOartWDErtP4oO7VwnNA8yJaO3bJcoGf_DiYuPw.svg%22%7D&auth_date=1745303756&signature=-Hr1Td7P0io1N2Cdp5GV5E-i7Omuv8k2FSIBmxTBXOviCLM5KXEBM9d81XQ3OD49OFKNzUc9n85UV_6NNwRQBw&hash=420898a7c85fc1a143b8ed34ee01356926d975e315dcdc58b51dec2dbdab376c"
 
