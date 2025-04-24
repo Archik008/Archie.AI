9239f19f013de07e555cb16a753a3f2e722ead5f
Артем <119063410+Archik008@users.noreply.github.com>
2025-04-24 20:51:35 +0500

    Test_password > password

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1,10 +1,10 @@
 import pytest
-from app.routes import TEST_PASSWORD
+from app.routes import PASSWORD
 from httpx import AsyncClient
 
 @pytest.mark.asyncio
 async def test_root(client: AsyncClient):
-    response = await client.get("/test_connection", params={"password": TEST_PASSWORD})
+    response = await client.get("/test_connection", params={"password": PASSWORD})
     assert response.status_code == 200
     data = response.json()
     assert "status" in data
