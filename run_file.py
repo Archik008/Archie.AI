df24e9779badf321a5258f5449a6e0a4f0ec41da
Артем <119063410+Archik008@users.noreply.github.com>
2025-04-29 08:57:07 +0500

    Fixed prompt with strong tag

--- run_file.py
+++ run_file.py
@@ -1,10 +1,11 @@
-from ai_dir.ai import QuizAi
+from ai_dir.ai import QuizAi, BibleChatAi
 
 def main():
-    prev_questions = ["Кто крестил Иисуса?", "Какое первое чудо совершил Иисус?"]
-    quiz = QuizAi.makeQuizAi(6, "Новый Завет", prev_questions)
+    # prev_questions = ["Кто крестил Иисуса?", "Какое первое чудо совершил Иисус?"]
+    # quiz = QuizAi.makeQuizAi(6, "Новый Завет", prev_questions)
 
-    print(quiz)
+    # print(quiz)
+    print(bot_output)
 
 if __name__ == '__main__':
     main()
