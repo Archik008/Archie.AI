7a20383f7ecf335c2efa37e3d1235fdf311c3cd5
Артем <119063410+Archik008@users.noreply.github.com>
2025-05-02 09:08:15 +0500

    Improved prev questions and prompts

--- run_test.py
+++ run_test.py
@@ -1,13 +1,12 @@
 from ai_dir.ai import QuizAi, BibleChatAi
 
 def main():
-    # prev_questions = ["Кто крестил Иисуса?", "Какое первое чудо совершил Иисус?"]
+    # prev_questions = ["Кто крестил Иисуса?", "Какое первое чудо совершил Иисус?", 'Где родился Иисус?']
     # quiz = QuizAi.makeQuizAi(6, "Новый Завет", prev_questions)
 
-    # print(quiz)
-    bot_output = BibleChatAi.askBibleChat("Честно ли будет использовать chatgpt в моем учебном проекте?", [], "Артем")
-
-    print(bot_output)
+    # bot_output = BibleChatAi.askBibleChat("Честно ли будет использовать chatgpt в моем учебном проекте?", [], "Артем")
+    pass
+    # print(bot_output)
 
 if __name__ == '__main__':
     main()
