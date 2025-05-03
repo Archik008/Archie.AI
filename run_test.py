614be8ff79e5d43af8a376d02267abcd92c0d4af
Артем <119063410+Archik008@users.noreply.github.com>
2025-05-03 19:34:39 +0500

    New functionality and new prompt in quiz ai!

--- run_test.py
+++ run_test.py
@@ -1,8 +1,8 @@
 from ai_dir.ai import QuizAi, BibleChatAi
 
 def main():
-    # prev_questions = ["Кто крестил Иисуса?", "Какое первое чудо совершил Иисус?", 'Где родился Иисус?']
-    # quiz = QuizAi.makeQuizAi(6, "Новый Завет", prev_questions)
+    prev_questions = ["Кто крестил Иисуса?", "Какое первое чудо совершил Иисус?", 'Где родился Иисус?']
+    quiz = QuizAi.makeQuizAi(6, "Новый Завет", prev_questions)
 
     # bot_output = BibleChatAi.askBibleChat("Честно ли будет использовать chatgpt в моем учебном проекте?", [], "Артем")
     pass
