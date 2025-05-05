932c7a1ac4e5db26d40cace480be325879a8d675
Артем <119063410+Archik008@users.noreply.github.com>
2025-05-05 17:34:29 +0500

    Updated prompt for bible chat

--- run_test.py
+++ run_test.py
@@ -1,12 +1,13 @@
 from ai_dir.ai import QuizAi, BibleChatAi
 
 def main():
-    prev_questions = ["Кто крестил Иисуса?", "Какое первое чудо совершил Иисус?", 'Где родился Иисус?']
-    quiz = QuizAi.makeQuizAi(6, "Новый Завет", prev_questions)
+    # prev_questions = ["Кто крестил Иисуса?", "Какое первое чудо совершил Иисус?", 'Где родился Иисус?']
+    # quiz = QuizAi.makeQuizAi(6, "Новый Завет", prev_questions)
 
-    # bot_output = BibleChatAi.askBibleChat("Честно ли будет использовать chatgpt в моем учебном проекте?", [], "Артем")
+    bot_output = BibleChatAi.askBibleChat("""Слушай, то что мне говорят постоянно неверующие часто на прощание "удачи", действительно ли удача есть или это ложь дьявола? Как мне это воспринимать?""", [], "Артем")
+    print(bot_output)
+
     pass
-    # print(bot_output)
 
 if __name__ == '__main__':
     main()
