3dcc7018aa6b93ab9e40085715b75aede92b285d
Артем <119063410+Archik008@users.noreply.github.com>
2025-05-05 17:49:39 +0500

    Fixed important prompt in bible chat ai

--- run_test.py
+++ run_test.py
@@ -4,8 +4,8 @@ def main():
     # prev_questions = ["Кто крестил Иисуса?", "Какое первое чудо совершил Иисус?", 'Где родился Иисус?']
     # quiz = QuizAi.makeQuizAi(6, "Новый Завет", prev_questions)
 
-    bot_output = BibleChatAi.askBibleChat("""Слушай, то что мне говорят постоянно неверующие часто на прощание "удачи", действительно ли удача есть или это ложь дьявола? Как мне это воспринимать?""", [], "Артем")
-    print(bot_output)
+    # bot_output = BibleChatAi.askBibleChat("""Если я использую chatgpt в проектах, то будет ли это честно?""", [], "Артем")
+    # print(bot_output)
 
     pass
 
